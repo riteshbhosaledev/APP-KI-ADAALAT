2bd77554b3bcc330d4571a65a41fa15f463c03a9
unknown <riteshbhosale2230@gmail.com>
2025-12-30 00:24:27 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyA1MJ_Aa7ve_yK_diKfrvVHaIhSoT_b6Nc",
+      appId: "1:478866771246:android:b8814bfe5fb13405babebf",
+      messagingSenderId: "478866771246",
+      projectId: "nyaay-dhrishti",
+    ),
+  );
   runApp(const MainApp());
 }
 
